2dbac9d1980041238ac311c21437c8648c515dac
Nick Tsourektsidis <creatorgr@hotmail.com>
2017-08-20 18:14:01 +0300

    Add font and basic markup

--- src/components/Receipt/index.js
+++ src/components/Receipt/index.js
@@ -1,5 +1,44 @@
 import React from 'react';
 
 export default function Receipt(props) {
-  return <div>Receipt here</div>;
+  return (
+    <section>
+      <h1>Your Order</h1>
+      <p>You have ordered the following items:</p>
+      
+      <ul>
+        <li>
+          <div>Banana</div>
+          <div>2.00</div>
+        </li>
+        <li>
+          <div>Tomato</div>
+          <div>1.00</div>
+        </li>
+        <li>
+          <div>Papaya</div>
+          <div>3.40</div>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <span>Sub Total: </span>
+          <span>6.40</span>
+        </li>
+        <li>
+          <span>Discount: </span>
+          <span>-0.00</span>
+        </li>
+        <li>
+          <span>Tax Amount: </span>
+          <span>1.40</span>
+        </li>
+        <li>
+          <span>Total: </span>
+          <span>7.80</span>
+        </li>
+      </ul>
+    </section>
+  );
 }
